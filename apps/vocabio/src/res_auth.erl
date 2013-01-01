c456ea184cc0672bebac22550ed7bcac3ba848df
Jan Daniel Bothma <jan.bothma@erlang-solutions.com>
2013-01-01 19:15:00 +0100

    Add delete button POSTing to /user/userid/word/wordid/delete
    ...for future delete operation.
    
    Also add base_url config option for making openid realm and return url

--- apps/vocabio/src/res_auth.erl
+++ apps/vocabio/src/res_auth.erl
@@ -1,7 +1,7 @@
 -module(res_auth).
 -export([init/3, handle/2, terminate/2]).
 
--define(RETURN_URL, "http://localhost:8080/auth/openid/return").
+-define(RETURN_URL(BaseURL), BaseURL++"auth/openid/return").
 
 init({tcp, http}, Req, _Opts) ->
     {ok, Req, undefined_state}.
@@ -19,8 +19,10 @@ request(_Method, [], Req) ->
     Assoc = openid:associate(AuthReq),
     {Session, Req1} = cowboy_session:from_req(Req),
     ok = vbo_session:set(Session, openid_assoc, Assoc),
-    Realm = "http://localhost:8080/",
-    AuthURL = openid:authentication_url(AuthReq, ?RETURN_URL, Realm, Assoc),
+    {ok, BaseURL} = application:get_env(vocabio, base_url),
+    Realm = BaseURL,
+    AuthURL =
+        openid:authentication_url(AuthReq, ?RETURN_URL(BaseURL), Realm, Assoc),
     ViewData = [{<<"auth_url">>, AuthURL}],
     {ok, RespBody} = home_dtl:render(ViewData),
     {RespBody, Req1};
@@ -29,7 +31,8 @@ request(_Method, [<<"return">>], Req) ->
     {QueryStringPairs, Req1} = cowboy_http_req:qs_vals(Req),
     {Session, Req2} = cowboy_session:from_req(Req1),
     {ok, Assoc} = vbo_session:get(Session, openid_assoc),
-    Bob = openid:verify(?RETURN_URL, Assoc, QueryStringPairs),
+    {ok, BaseURL} = application:get_env(vocabio, base_url),
+    Bob = openid:verify(?RETURN_URL(BaseURL), Assoc, QueryStringPairs),
     {_, OpenIDIdentity} = lists:keyfind(<<"identity">>, 1, Bob),
     case vbo_model_user:get_id_by_openid(OpenIDIdentity) of
         {ok, notfound} ->
